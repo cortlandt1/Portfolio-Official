03874e39ca8cf71f946f334ed023f4dfb4557d7d
Gordo <cortland.taylor1@gmail.com>
2019-08-20 18:06:34 -0700

    Updates

---
+++ precache-manifest.43a67b70754accbf93e56217337afdde.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c043bcf523798d53ea8367235416e62",
+    "url": "/Portfolio-Official/index.html"
+  },
+  {
+    "revision": "ec477851fd56be471af8",
+    "url": "/Portfolio-Official/static/css/main.5c1b6520.chunk.css"
+  },
+  {
+    "revision": "9430c51be7d9235c45b4",
+    "url": "/Portfolio-Official/static/js/2.618a59e0.chunk.js"
+  },
+  {
+    "revision": "ec477851fd56be471af8",
+    "url": "/Portfolio-Official/static/js/main.8d9289b5.chunk.js"
+  },
+  {
+    "revision": "92d4ee1057d8588062a6",
+    "url": "/Portfolio-Official/static/js/runtime~main.3c2c6af9.js"
+  },
+  {
+    "revision": "81dc61b137dc9d3c83c32912e06247dd",
+    "url": "/Portfolio-Official/static/media/Trail-img.81dc61b1.jpg"
+  },
+  {
+    "revision": "bb4f3d86bba57ea1144c47cf64762392",
+    "url": "/Portfolio-Official/static/media/yosemite2.bb4f3d86.jpg"
+  }
+]);
